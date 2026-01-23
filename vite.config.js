5e381d288b8a2b6195224227ecf308f090dc4999
CARLOS ANDRE PEREIRA DE OLIVEIRA <72883380+EEMTIDMA@users.noreply.github.com>
2026-01-23 16:50:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/meu-orcamento-familiar-andre/', 
+  base: '/meu-orcamento-familiar-andre/', // Adicione esta linha com o nome exato do repositório
 })
